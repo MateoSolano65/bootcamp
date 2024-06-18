bc38d83f05581c0c519311fbc097767a6188cdbf
Mateo Solano <mateosolano65@gmail.com>
2024-06-18 11:17:03 -0500

    refactor: Update sidebar component and page structure

--- module_I/lab1/src/components/sidebar/page.jsx
+++ module_I/lab1/src/components/sidebar/page.jsx
@@ -1,30 +1,80 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import Link from 'next/link';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
 
-export function Sidebar() {
+export const Sidebar = ({ children }) => {
   return (
-    <>
-      <Nav
-        className="col-md-12 d-none d-md-block bg-light sidebar"
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
-        <div className="sidebar-sticky"></div>
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </>
+    <div className="container-fluid">
+      <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 pc-0 bg-dark">
+          <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+            <Link
+              href="/admin"
+              className="d-flex align-items-center pb-3 mb-md-1 me-md-auto text-white text-decoration-none"
+            >
+              <span className="fs-5 fw-bolder d-none d-sm-inline">CWC</span>
+            </Link>
+            <ul
+              className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
+              id="menu"
+            >
+              <li className="w-100">
+                <Link
+                  href="/admin/dashboard"
+                  className="nav-link text-white px-0 align-middle"
+                >
+                  <i className="fs-4 bi-speedometer2 ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Dashboard</span>
+                </Link>
+              </li>
+              <li className="w-100">
+                <Link
+                  href="/admin/employees"
+                  className="nav-link px-0 align-middle text-white"
+                >
+                  <i className="fs-4 bi-people ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">
+                    Manage Employees
+                  </span>
+                </Link>
+              </li>
+              <li className="w-100">
+                <Link
+                  href="/admin/categories"
+                  className="nav-link px-0 align-middle text-white"
+                >
+                  <i className="fs-4 bi-columns ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Categories</span>
+                </Link>
+              </li>
+              <li className="w-100">
+                <Link
+                  href="/admin/profile"
+                  className="nav-link px-0 align-middle text-white"
+                >
+                  <i className="fs-4 bi-person ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Profile</span>
+                </Link>
+              </li>
+              <li className="w-100">
+                <Link
+                  href="/admin/login"
+                  className="nav-link px-0 align-middle text-white"
+                >
+                  <i className="fs-4 bi-box-arrow-right ms-2"></i>
+                  <span className="ms-2 d-none d-sm-inline">Logout</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="col p-0 m-0">
+          <div className="p-2 d-flex justify-content-center shadow">
+            <h3>Employee Management System</h3>
+          </div>
+          {children}
+        </div>
+      </div>
+    </div>
   );
-}
+};
